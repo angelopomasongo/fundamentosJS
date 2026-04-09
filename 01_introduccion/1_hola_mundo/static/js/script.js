b8c89e1b11e6ca13f8d4e7538ab87b1b198dbd02
angelopomasongo <angelopomasongo@liceovvh.cl>
2026-04-09 12:58:37 -0400

    .

---
+++ 01_introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,12 @@
+// Prueba de conexion con js 
+console.log("Conexion exitosa con js externo");
+
+//(1)Hola mundo con variable y const.
+const saludo = "hola a todos, ";
+let destinatario = "Estudiantes, ";
+//impresion de saludo concatenando con (+)
+console.log(saludo + "para los " + destinatario);
+//(2) Saludar a una persona ingresada por teclado
+let nombre = prompt("ingrese su nombre: ")
+//imprimir un nombre ingresado con prompt
+console.log("hola," + nombre);
---
+++ 02_Tipos_de_datos/1-tipo_texto/static/js/script.js
@@ -0,0 +1,9 @@
+console.log("conexion con js correcta...");
+/* 
+concatenacion de texto con el signo +
+podemos unir texto y variables
+*/
+const nombre = "Angelo";
+const apellido = "Pomasongo";
+//unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es: " + nombre + " " + apellido);
